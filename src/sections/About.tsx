83326c02a8e867f5e566ae1e992ac3d4b0f42f33
Fernando <fernandozimmermannmaciel@gmail.com>
2025-01-08 19:44:06 -0300

    solve some issues with home page

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,131 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import { TechIcon } from "@/components/TechIcon";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import mapImage from "@/assets/images/map.png";
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/ToolboxItems";
+
+const toolboxItems = [
+ {
+  title: 'Java',
+  iconType: JavascriptIcon,
+ },
+ {
+  title: 'SpringBoot',
+  iconType: JavascriptIcon,
+ },
+ {
+  title: 'JavaScript',
+  iconType: JavascriptIcon,
+ },
+ {
+  title: 'HTML5',
+  iconType: JavascriptIcon,
+ },
+ {
+  title: 'TailwindCSS',
+  iconType: JavascriptIcon,
+ },
+ {
+  title: 'Github',
+  iconType: JavascriptIcon,
+ },
+]
+
+const hobbies = [
+  {
+    title: 'VideoGames',
+    emoji: '🎮',
+    left: '5%',
+    top: '5%'
+  },
+  {
+    title: 'Anime',
+    emoji: '🍙',
+    left: '50%',
+    top: '5%'
+  },
+  {
+    title: 'Piano',
+    emoji: '🎹',
+    left: '35%',
+    top: '40%'
+  },
+  {
+    title: 'Vôlei',
+    emoji: '🏐',
+    left: '70%',
+    top: '45%'
+  },
+  {
+    title: 'Gatos',
+    emoji: '🐱‍👤',
+    left: '5%',
+    top: '65%'
+  },
+  {
+    title: 'Matemática',
+    emoji: '➗',
+    left: '10%',
+    top: '35%'
+  },
+  {
+    title: 'Rock and Roll',
+    emoji: '🎸',
+    left: '45%',
+    top: '70%'
+  }
+]
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20 lg:py-28">
+      <div className="container">
+        <SectionHeader title={"Um Pouco do Meu Mundo"} eyebrow={"Sobre Mim"} description={"Conheça um pouco mais sobre que eu sou, o que eu faço e o que me inspira."} />
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader title={"Leitura"} description={"Explore os livros que moldam quem eu sou."}/>
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Capa do Livro"/>
+              </div>
+            </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+            <CardHeader className="" title={"Tecnologias"} description={"Explore as tecnologias e ferramentas que utilizo para criar experiências digitais."}/>
+              <ToolboxItems className="" items={toolboxItems} />
+              <ToolboxItems className="mt-6" itemsWrapperClassName="-translate-x-1/2" items={toolboxItems} />
+            </Card>
+          </div>
+          <div className="grid gap-8 grid-cols-1 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+            <CardHeader title={"Por Trás do Código"} description={"Explore meus interesses e hobbies."} className="px-6 pt-6"/>  
+              <div className="relative flex-1">
+                {hobbies.map(hobby => (
+                  <div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                  style={{
+                    left: hobby.left,
+                    top: hobby.top,
+                  }}>
+                    <span className="font-medium text-gray-950">{hobby.title}</span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+              <Image className="h-full w-full object-cover object-left-top" src={mapImage} alt="mapa" />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <Image className="size-20" src={smileMemoji} alt="memoji sorrindo"/>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,48 +1,35 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import animeTrackerPage from "@/assets/images/anime-tracker-page.png";
+import meetupPage from "@/assets/images/meet-up-page.png";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
 import grainImage from "@/assets/images/grain.jpg";
-import { url } from "inspector";
+import { SectionHeader } from "@/components/SectionHeader";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "MeetUp",
+    year: "2024",
+    title: "MeetUp Spot",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Encontre o melhor lugar para se encontrar!" },
+      { title: "Aplicação acelerada por IA" },
+      { title: "Funcionalidade baseada em Geometria Não-Euclidiana" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    link: "https://portfolio-fernando-3za5.vercel.app/",
+    image: meetupPage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: "Anime Tracker",
+    year: "2024",
+    title: "Anime Tracker",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: "Organize os seus Animes favoritos!" },
+      { title: "Integrado com o maior site de Animes do mundo" },
+      { title: "Design clean e responsivo" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
-  },
-  {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    link: "https://anime-tracker-vert.vercel.app/",
+    image: animeTrackerPage,
   },
 ];
 
@@ -50,11 +37,8 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">Resultados Reais</p>
-        </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Projetos em Destaque</h2>
-        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">Veja como eu transformei conceitos em experiências digitais</p>
+        <SectionHeader title={"Resultados Reais"} eyebrow={"Projetos em Destaque"} description={"Veja como eu transformei conceitos em experiências digitais"}/>
+
         <div className="flex flex-col mt-10 md:mt-20  gap-20">
           {portfolioProjects.map(project => (
             <div key={project.title} className="lg:pt-16 lg:px-20 md:pt-12 md:px-10 px-8 pt-8 bg-gray-800 rounded-3xl z-0 overflow-hidden after:z-10 relative after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none">
@@ -70,7 +54,7 @@ export const ProjectsSection = () => {
                     <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
                     <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                       {project.results.map(result =>(
-                        <li className="flex gap-2 text-sm md:text-base text-white/50">
+                        <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50">
                           <CheckCircleIcon className="size-5 md:size-6"/>
                           <span>{result.title}</span>
                         </li>
